f3bd4a7e774b5f5aaedf294240518628103c327b
oplustil-prog <oplustil@kaiserservis.cz>
2026-06-28 12:41:16 +0200

    fix: serve data box route without redirect

--- functions/datova-schranka/[[path]].js
+++ functions/datova-schranka/[[path]].js
@@ -1,5 +1,5 @@
 export async function onRequestGet({ request, env }) {
-  const indexUrl = new URL("/index.html", request.url);
+  const indexUrl = new URL("/", request.url);
 
   if (env?.ASSETS?.fetch) {
     return env.ASSETS.fetch(indexUrl);
