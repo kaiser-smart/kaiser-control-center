ad104ca12efc6cee2a48dd466018d11203725072
martinkonecek-source <232428047+martinkonecek-source@users.noreply.github.com>
2026-07-06 11:43:39 +0200

    Use inactive T-Cars style tracking hero icon

--- src/components/icons/VehicleTrackingIcon.js
+++ src/components/icons/VehicleTrackingIcon.js
@@ -1,3 +1,3 @@
 export function VehicleTrackingIcon() {
-  return '<img class="module-icon__svg" src="/design-icons/017.svg" alt="" aria-hidden="true" loading="lazy">';
+  return '<img class="module-icon__svg" src="/design-icons/018.svg" alt="" aria-hidden="true" loading="lazy">';
 }
